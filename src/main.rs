15bf61be26edcdb8dbc9eb294267b6259adb5da6
UseK <use.k.james@gmail.com>
2022-01-03 16:16:42 +0900

    Show Chart.js with Tera

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,31 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
+use std::collections::HashMap;
+
+use rocket::{Build, Rocket};
+use rocket_dyn_templates::Template;
+
+extern crate tera;
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> Template {
+    let mut context: HashMap<String, String> = HashMap::new();
+    context.insert("name".to_string(), "Tera".to_string());
+    Template::render("index", &context)
+}
+
+#[get("/<name>")]
+fn insert_template(name: &str) -> Template {
+    println!("{}", name);
+    let mut context: HashMap<String, String> = HashMap::new();
+    context.insert("name".to_string(), name.to_string());
+    Template::render("index", &context)
 }
 
 #[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+fn rocket() -> Rocket<Build> {
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/", routes![insert_template])
+        .attach(Template::fairing())
 }
