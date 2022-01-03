6baac7c6a73cbbbeb3647ac60a1609c69edd633c
UseK <use.k.james@gmail.com>
2022-01-03 16:54:46 +0900

    Snapshot: introduce sqlite

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,11 @@
 extern crate rocket;
 use std::collections::HashMap;
 
+use r2d2::Pool;
+use r2d2_sqlite::SqliteConnectionManager;
 use rocket::{Build, Rocket};
 use rocket_dyn_templates::Template;
+use rusqlite::params;
 
 extern crate tera;
 
@@ -24,6 +27,19 @@ fn insert_template(name: &str) -> Template {
 
 #[launch]
 fn rocket() -> Rocket<Build> {
+    let manager = SqliteConnectionManager::file("payment.db");
+    let pool = Pool::new(manager).expect("Fail to init pool");
+    let conn = pool.get().expect("Fail to get connection from pool");
+    conn.execute(
+        "create table if not exists payment (
+            date text not null,
+            name text not null,
+            value integer
+        )",
+        params![],
+    )
+    .expect("Failed to create table");
+
     rocket::build()
         .mount("/", routes![index])
         .mount("/", routes![insert_template])
